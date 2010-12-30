d360cc8081f95aab023d07906b2d1b1c03f6b474
David Sansome <me@davidsansome.com>
2010-12-30 14:34:50 +0000

    Copyright headers and header guards

--- src/updatedialog.h
+++ src/updatedialog.h
@@ -1,22 +1,27 @@
-/* This file is part of Clementine.
-   Copyright 2010, David Sansome <me@davidsansome.com>
+/* This file is part of qtsparkle.
+   Copyright (c) 2010 David Sansome <me@davidsansome.com>
 
-   Clementine is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
 
-   Clementine is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
 
-   You should have received a copy of the GNU General Public License
-   along with Clementine.  If not, see <http://www.gnu.org/licenses/>.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
 */
 
-#ifndef UPDATEDIALOG_H
-#define UPDATEDIALOG_H
+#ifndef QTSPAKRLE_UPDATEDIALOG_H
+#define QTSPAKRLE_UPDATEDIALOG_H
 
 #include "appcast.h"
 
@@ -51,4 +56,4 @@ private:
 
 } // namespace qtsparkle
 
-#endif // UPDATEDIALOG_H
+#endif // QTSPAKRLE_UPDATEDIALOG_H
